98a78a92941509262829b66a02c1fb435fbd6440
Rafał Żemojtel <rafal.zemojtel@sagiton.pl>
2021-12-05 12:48:56 +0100

    Updates

---
+++ sw.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('install', async () => {
+})
+
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('activate', event => {
+})
