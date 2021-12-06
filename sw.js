aa4e08339d9971bdc00f97754c7fd941c93c7931
Rafał Żemojtel <rafal.zemojtel@sagiton.pl>
2021-12-06 08:40:02 +0100

    Updates

--- sw.js
+++ sw.js
@@ -1,32 +1,26 @@
-const CACHE_NAME = 'd2r-holy-grail-cache-v1';
-const URLS_TO_CACHE = [
+const CACHE_NAME = 'd2r-holy-grail-cache-v2';
+const FILES_TO_CACHE = [
   '/',
   '/index.html',
   '/images/d2r_logo.gif'
 ];
 
 // eslint-disable-next-line no-restricted-globals
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(URLS_TO_CACHE);
-      })
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
   // eslint-disable-next-line no-restricted-globals
   self.skipWaiting();
 });
 
 // eslint-disable-next-line no-restricted-globals
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-          if (response) {
-            return response;
-          }
-          return fetch(event.request);
-        }
-      )
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
   );
 });
