9a4ecd4de6409091fe73f430a09c1971cfc7e0b4
Rafał Żemojtel <rafal.zemojtel@sagiton.pl>
2021-12-14 11:36:11 +0100

    Items update

--- src/utils/itemHelpers.ts
+++
@@ -1,9 +0,0 @@
-export const getPropertyRangeOrValue = (values: number[] | undefined): string => {
-  if (!values || !Array.isArray(values) || values.length === 0) {
-    return '';
-  }
-
-  const isSame = values[0] === values[1];
-
-  return isSame ? `${values[0]}` : `${values[0]}-${values[1]}`;
-};
